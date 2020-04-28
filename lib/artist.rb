e1b4fb5f52ab6671bb0758be98fe6b68ca03d1e6
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-28 23:40:25 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,6 @@
+class Artist 
+  attr_accessor :name 
+  def initalize (name)
+    @name = name
+  end
+end
---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post 
+  attr_accessor :title, :author 
+  def initalize (title, author)
+    @title = title 
+    @author = author 
+  end 
+end 
